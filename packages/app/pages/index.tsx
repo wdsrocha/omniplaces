b9d8152e0bde81e467048b1028130bf5af603c76
Wesley Rocha <wesleysr1997@gmail.com>
2020-10-18 23:39:12 -0400

    Init frontend from nextjs example
    The example was with-typescript-eslint-jest.

---
+++ packages/app/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export const Home = (): JSX.Element => (
+  <>
+    <Head>
+      <title>Omniplaces</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main>Hello, world!</main>
+  </>
+)
+
+export default Home
