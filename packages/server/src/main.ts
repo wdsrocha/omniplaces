d9dcfc99902484601a655b67d10b1db01e044fab
Wesley Rocha <wesleysr1997@gmail.com>
2020-10-23 04:47:51 -0400

    Enable CORS in server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,6 +3,7 @@ import { PlacesModule } from './places/places.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PlacesModule);
+  app.enableCors();
   await app.listen(2000);
 }
 bootstrap();
